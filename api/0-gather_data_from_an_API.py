8a3ad9cb1ecf1781fabe06d816f13876e2ba886d
Trey Turner <trey.turner1857@gmail.com>
2024-07-11 11:30:16 -0500

    fixing some indentation problems.

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,34 +14,29 @@ def get_employee_todo_progress(employee_id):
     # Make the GET request
     response = requests.get(url)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        todos = response.json()
-        total_tasks = len(todos)
-        done_tasks = sum(1 for todo in todos if todo['completed'])
+    # Extracting employee name
+    employee_data = requests.get(employee_url).json()
+    employee_name = employee_data.get('name')
 
-        # Extracting employee name
-        employee_data = requests.get(employee_url).json()
-        employee_name = employee_data.get('name')
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, done_tasks, total_tasks))
 
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee_name, done_tasks, total_tasks))
+    todos_response = requests.get(
+        "{}/todos".format(base_url), params={'userId': employee_id}
+    )
+    todos_data = todos_response.json()
 
-        todos_response = requests.get(
-            "{}/todos".format(base_url), params={'userId': employee_id}
-        )
-        todos_data = todos_response.json()
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task.get('completed')]
+    number_of_done_tasks = len(done_tasks)
 
-        total_tasks = len(todos_data)
-        done_tasks = [task for task in todos_data if task.get('completed')]
-        number_of_done_tasks = len(done_tasks)
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name, number_of_done_tasks, total_tasks)
+    )
 
-        print(
-            "Employee {} is done with tasks({}/{}):".format(
-                employee_name, number_of_done_tasks, total_tasks)
-        )
-        for task in done_tasks:
-            print("\t {}".format(task.get('title')))
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
 
 
 if __name__ == "__main__":
