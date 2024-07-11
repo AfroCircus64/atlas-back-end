6c93c4c37764241ee0927c1fe97d2d5d8b0fbf54
Trey Turner <trey.turner1857@gmail.com>
2024-07-11 11:28:21 -0500

    Re-did some code. (untested)

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -27,12 +27,21 @@ def get_employee_todo_progress(employee_id):
         print("Employee {} is done with tasks({}/{}):".format(
             employee_name, done_tasks, total_tasks))
 
-        # Printing each completed task title
-        for todo in todos:
-            if todo['completed']:
-                print("\t{}".format(todo['title']))
-    else:
-        print("Failed to retrieve data")
+        todos_response = requests.get(
+            "{}/todos".format(base_url), params={'userId': employee_id}
+        )
+        todos_data = todos_response.json()
+
+        total_tasks = len(todos_data)
+        done_tasks = [task for task in todos_data if task.get('completed')]
+        number_of_done_tasks = len(done_tasks)
+
+        print(
+            "Employee {} is done with tasks({}/{}):".format(
+                employee_name, number_of_done_tasks, total_tasks)
+        )
+        for task in done_tasks:
+            print("\t {}".format(task.get('title')))
 
 
 if __name__ == "__main__":
