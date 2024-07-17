1d417fc79042aa361307be0c6ccf2e9ba6fb0479
Trey Turner <trey.turner1857@gmail.com>
2024-07-16 23:18:38 -0500

    PEP8 fixes

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -4,12 +4,14 @@
 import json
 import requests
 
+
 def fetch_employee_data(employee_id):
     """Fetches employee data including username"""
     base_url = "https://jsonplaceholder.typicode.com"
     employee_url = f"{base_url}/users/{employee_id}"
     response = requests.get(employee_url)
     return response.json()
+
 
 def fetch_todo_data(employee_id):
     """Fetches todo data for a given employee ID"""
@@ -18,24 +20,25 @@ def fetch_todo_data(employee_id):
     response = requests.get(todos_url)
     return response.json()
 
+
 def export_to_json():
     """Exports tasks for all employees to a JSON file"""
     all_employees_data = {}
-    
+
     # Assuming there are 10 users as per the example output
     for employee_id in range(1, 11):
         employee_data = fetch_employee_data(employee_id)
         todos_data = fetch_todo_data(employee_id)
         username = employee_data.get('username')
-        
+
         tasks = [{
             "task": todo['title'],
             "completed": todo['completed'],
             "username": username
         } for todo in todos_data]
-        
+
         all_employees_data[str(employee_id)] = tasks
-    
+
     with open('todo_all_employees.json', 'w') as file:
         json.dump(all_employees_data, file)
 
